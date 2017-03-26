f0cf5a32852d3ff2fb289325b56efb87505f59f9
Ana Brassard <ana.brassard@fer.hr>
2017-03-26 18:49:38 +0200

    no changes to programs

--- SenseHive/temp_Grouping.py
+++ SenseHive/temp_Grouping.py
@@ -15,57 +15,60 @@ class MPVSM:
     # only one sense per word for now
     # TODO: smart way to find ideal # of clusters
     def getBestK(self):
-        return 1
+        bestK = 1
+        print "best K:", bestK
+        return bestK
 
     # for each word in vocabulary, cluster occurences.
     # every centroid then represents a sense; multiple senses per word
     def getSenses(self):
-        for word in self.dataset.vocab:
-            X = self.dataset.occurences[word]
-            if len(X) >= self.getBestK():
-                km = KMeans(n_clusters=self.getBestK(), init='k-means++').fit(X)
-                self.senses[word] = km.cluster_centers_
+        K = self.getBestK()
+        i=0
+        with progressbar.ProgressBar(max_value=50) as bar:
+            for word in self.dataset.vocab:
+                i+=1
+                if i%(len(self.dataset.vocab)/50) == 0:
+                    bar.update(i)
+                X = self.dataset.occurences[word]
+                if len(X) >= K:
+                    km = KMeans(n_clusters=K, init='k-means++').fit(X)
+                    self.senses[word] = km.cluster_centers_
             
     # find n most similar words to given word (target)
-    # TODO: some smarter way
     def findSimilar(self, target, n, mode='AVG'):
         distances = []
         targetSenses = self.senses[target]
-
-        similarity = lambda u,v: np.dot(u, v)/(math.sqrt(np.dot(u, u)) * math.sqrt(np.dot(v, v)))
         for word in self.dataset.vocab:
-            similarities = [similarity(u,v) for u in targetSenses for v in self.senses[word]]
+            similarities = sp.spatial.distance.cdist(targetSenses, self.senses[word])
             if mode=='AVG':
-                sim = sum(similarities)/len(targetSenses)**2
+                sim = float(np.avg(similarities))
             elif mode=='MAX':
-                sim = max(similarities)
+                sim = float(max(similarities))
             distances.append(sim)
-
-        mostSimilar = list(np.array(self.dataset.vocab)[np.argsort(distances)[-n-1:]])
-        mostSimilar.remove(target)
-        return mostSimilar[::-1] #!!!!!!!!!!!!!!!
+        mostSimilar = list(np.array(self.dataset.vocab)[np.array(distances).argsort()[::-1][:n+1]])
+        if target in mostSimilar:
+            mostSimilar.remove(target)
+        return mostSimilar
 
     # get n random words, print m most similar
     def test(self, n, m, mode):
-        #out = codecs.open('out.txt', 'w')
-        #out.write("TEST: "+str(n)+" random words, "+str(m)+" most similar\n\n")
         print "TEST: "+str(n)+" random words, "+str(m)+" most similar\n\n"
-        sample = np.random.choice(self.dataset.vocab[50:500], n)
+        sample = np.random.choice(self.dataset.vocab[50:300], n)
         for word in sample:
             similar = model.findSimilar(word, m, mode)
-            #out.write('['+word+']' + '\n' + ' '.join(similar)+'\n\n')
             print '['+word+']' + '\n' + ' '.join(similar)+'\n'
-        #out.close()
 
   
 if __name__ == "__main__":
 
     model = MPVSM()
-    #model.dataset = Data("data/Picard.txt") # for fast testing. text too short for meaningful results
     # options: picard.txt, nahodi.txt, 1984.txt
+    #model.dataset = Data("data/Picard.txt") # for fast testing. text too short for meaningful results
     model.dataset = Data("data/nahodi.txt", n=10, truncate=30000) # must truncate because memory error :<
+    print "Data ready."
     print "10 most common words:", ', '.join(model.dataset.vocab[:10])
+    print "Vocabulary size:", len(model.dataset.vocab)
+    print "Calculating senses"
     model.getSenses()
-    model.test(20, 5, 'MAX')
-
-    # Trenutni rezultat: vrati one rijeci s kojima je najcesce bila u recenici (npr djeca -> napustena)
+    print "Done"
+    model.test(10, 5, 'MAX')
